175d35ec3895f26e1121ee321d1761ca0c3395b6
jhadev <joshappeldev@gmail.com>
2020-06-01 19:59:04 -0400

    add json db to test fs read/write

--- controllers/person-controller.ts
+++ controllers/person-controller.ts
@@ -1,21 +1,14 @@
 import { Person } from '../models/person.ts';
+import { readJson, writeJson } from 'https://deno.land/std/fs/mod.ts';
 
-export let people: Person[] = [
-  {
-    name: 'Ian',
-    age: 30,
-  },
-  {
-    name: 'Ryan',
-    age: 28,
-  },
-];
+const db = './db.json';
 
-export const getPeople = ({ response }: { response: any }) => {
-  response.body = people;
+export const getPeople = async ({ response }: { response: any }) => {
+  console.log(await readJson(db));
+  response.body = await readJson(db);
 };
 
-export const getPerson = ({
+export const getPerson = async ({
   params,
   response,
 }: {
@@ -24,6 +17,7 @@ export const getPerson = ({
   };
   response: any;
 }) => {
+  const people = (await readJson(db)) as Person[];
   const person = people.find(
     (person: Person) => person.name.toLowerCase() === params.name
   );
@@ -44,12 +38,16 @@ export const addPerson = async ({
   request: any;
   response: any;
 }) => {
+  const people = (await readJson(db)) as Person[];
   const body = await request.body();
   const { name, age }: { name: string; age: number } = body.value;
+
   people.push({
     name: name,
     age: age,
   });
+
+  await writeJson(db, people, { spaces: 2 });
 
   response.body = { msg: 'OK', data: people };
   response.status = 200;
@@ -66,24 +64,33 @@ export const updatePerson = async ({
   request: any;
   response: any;
 }) => {
-  const temp = people.find(
-    (person: Person) => person.name.toLowerCase() === params.name
-  );
+  const people = (await readJson(db)) as Person[];
   const body = await request.body();
   const { age }: { age: number } = body.value;
 
-  if (temp) {
-    temp.age = age;
-    response.status = 200;
-    response.body = { msg: 'OK', data: temp };
+  if (!people.length) {
+    response.status = 400;
+    response.body = { msg: `Cannot find person ${params.name}` };
     return;
   }
 
-  response.status = 400;
-  response.body = { msg: `Cannot find person ${params.name}` };
+  const updated = people.map((person: Person) => {
+    if (person.name.toLowerCase() === params.name) {
+      person.age = age;
+    }
+    return person;
+  });
+
+  let res = updated.filter(
+    (person: Person) => person.name.toLowerCase() === params.name
+  );
+
+  await writeJson(db, updated, { spaces: 2 });
+  response.status = 200;
+  response.body = { msg: 'OK', data: res };
 };
 
-export const removePerson = ({
+export const removePerson = async ({
   params,
   response,
 }: {
@@ -92,6 +99,7 @@ export const removePerson = ({
   };
   response: any;
 }) => {
+  let people = (await readJson(db)) as Person[];
   const lengthBefore = people.length;
   people = people.filter(
     (person: Person) => person.name.toLowerCase() !== params.name
@@ -103,6 +111,7 @@ export const removePerson = ({
     return;
   }
 
+  await writeJson(db, people, { spaces: 2 });
   response.body = { msg: 'OK', data: people };
   response.status = 200;
 };
